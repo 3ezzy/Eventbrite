f66498f68f75789ae4417bad3eea00ceda73cb20
Achraf <hanzazachraf581@gmail.com>
2025-02-12 16:57:26 +0100

    Added the connection to the database

---
+++ app/Database/Database.php
@@ -0,0 +1,38 @@
+<?php
+
+class Database {
+    private static ?Database $instance = null;
+    private PDO $pdo;
+
+    private function __construct() {
+        $configFile = __DIR__ . '/../config/database.php';
+
+        if (!file_exists($configFile)) {
+            throw new Exception('Le fichier de configuration de la base de données est manquant.');
+        }
+
+        $config = require $configFile;
+
+        try {
+            $this->pdo = new PDO(
+                "pgsql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}",
+                $config['user'],
+                $config['password'],
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+            );
+        } catch (PDOException $e) {
+            throw new Exception("Erreur de connexion : " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance(): PDO {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance->pdo;
+    }
+
+    private function __clone() {}
+
+    private function __wakeup() {}
+}
--- app/config/database.php
+++ app/config/database.php
@@ -1,23 +1,8 @@
-// app/config/Database.php
 <?php
-class Database {
-    private static $instance = null;
-    private $pdo;
-
-    private function __construct() {
-        $config = require 'database.php';
-        $this->pdo = new PDO(
-            "pgsql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}",
-            $config['user'],
-            $config['password']
-        );
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-
-    public static function getInstance() {
-        if (self::$instance === null) {
-            self::$instance = new self();
-        }
-        return self::$instance->pdo;
-    }
-}
+return [
+    'host' => 'localhost',
+    'port' => '5432',
+    'dbname' => 'youevent',
+    'user' => 'postgres',
+    'password' => 'password'
+];
